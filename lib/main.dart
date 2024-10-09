087ff3f4a3a9f6c0597ab420ee01bf5300f37f46
Tomislav <tojaku@gmail.com>
2024-10-09 11:36:24 +0200

    prvi commit

---
+++ lib/main.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: MojObrazac(),
+        ),
+      ),
+    );
+  }
+}
+
+class MojGumb extends StatelessWidget {
+  const MojGumb({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+        onPressed: () {
+          debugPrint('Gumb pritisnut!');
+        },
+        child: const Text('Pritisni me'));
+  }
+}
+
+class MojObrazac extends StatelessWidget {
+  MojObrazac({super.key});
+
+  final TextEditingController nameController = TextEditingController();
+  final TextEditingController surnameController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text(
+            'Unesite svoje ime i prezime',
+            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(
+            height: 32,
+          ),
+          TextField(
+            controller: nameController,
+            decoration: const InputDecoration(
+                labelText: 'Ime', border: OutlineInputBorder()),
+          ),
+          const SizedBox(
+            height: 16,
+          ),
+          TextField(
+            controller: surnameController,
+            decoration: const InputDecoration(
+                labelText: 'Prezime', border: OutlineInputBorder()),
+          ),
+          const SizedBox(
+            height: 16,
+          ),
+          ElevatedButton(
+              onPressed: () {
+                String ime = nameController.text;
+                String prezime = surnameController.text;
+                debugPrint('Ime: $ime, Prezime: $prezime');
+              },
+              child: const Text('Potvrdi'))
+        ],
+      ),
+    );
+  }
+}
